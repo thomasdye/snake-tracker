2fad49599525e630f272f5d48845155188e95dfa
Thomas <thomasallendye@gmail.com>
2021-01-23 21:32:39 -0500

    initial commit
    created save and load function for new snakes. minimal storyboarding done.

---
+++ Snake Tracker/Snake Tracker/Models/Snake.swift
@@ -0,0 +1,17 @@
+//
+//  Snake.swift
+//  Snake Tracker
+//
+//  Created by Thomas Dye on 1/23/21.
+//
+
+import Foundation
+
+struct Snake: Codable {
+  var name: String
+  var age: String
+  var type: String
+  var gender: String
+}
+
+
